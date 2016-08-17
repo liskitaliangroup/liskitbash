ba456d311c4ce4935396087ec70dfd8abaff6935
andreafspeziale <andreaf.speziale@gmail.com>
2016-08-18 00:40:12 +0200

    testing params

--- liskitbash.sh
+++ liskitbash.sh
@@ -1,29 +1,40 @@
 #!/bin/bash
 
+#$1 command
+#$2 type
+#$3 link
+
+# Installing a brand new version
 install ()
 {
-    echo You runned install
+    echo install
+    echo $1
+    echo $2
+    echo $3
 }
 
 # Updating existing Lisk version
 update ()
 {
-    echo Updating Lisk
-    echo Stopping Lisk...
-    cd lisk-$2
-    bash lisk.sh stop
-    cd ..
-    echo Deleting Lisk
-    rm -r lisk-$2
-    rm install installLisk.sh
-    echo Preparing system 4 Lisk upgrade
-    sudo apt-get --purge remove postgresql postgresql postgresql-client postgresql-client postgresql-client-common postgresql-common postgresql-contrib postgresql-contrib
-    rm -rf /var/lib/postgresql/
-    sudo rm -rf /var/log/postgresql/
-    sudo rm -rf /etc/postgresql/
-    echo Reinstalling Lisk
-    wget https://downloads.lisk.io/scripts/installLisk.sh
-    bash installLisk.sh install -r $2
+    echo update
+    echo $1
+    echo $2
+    echo $3
+    # echo Stopping Lisk...
+    # cd lisk-$2
+    # bash lisk.sh stop
+    # cd ..
+    # echo Deleting Lisk
+    # rm -r lisk-$2
+    # rm install installLisk.sh
+    # echo Preparing system 4 Lisk upgrade
+    # sudo apt-get --purge remove postgresql postgresql postgresql-client postgresql-client postgresql-client-common postgresql-common postgresql-contrib postgresql-contrib
+    # rm -rf /var/lib/postgresql/
+    # sudo rm -rf /var/log/postgresql/
+    # sudo rm -rf /etc/postgresql/
+    # echo Reinstalling Lisk
+    # wget https://downloads.lisk.io/scripts/installLisk.sh
+    # install()
 }
 
 # Checking Options
