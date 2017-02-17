9f8ab99e0705b7f852d0d7a6bb267e0e3af15baf
andreafspeziale <andreaf.speziale@gmail.com>
2017-02-17 10:17:08 +0100

    Update liskitbash.sh

--- liskitbash.sh
+++ liskitbash.sh
@@ -32,6 +32,8 @@ install ()
     wget https://downloads.lisk.io/scripts/installLisk.sh
     echo -e ${GREEN}Running Lisk binary install script...${NC}
     bash installLisk.sh install -r $NETWORK
+    cd lisk-$NETWORK
+    bash lisk.sh coldstart
     rm installLisk.sh
     echo -e ${GREEN}Lisk sould be installed...${NC}
 }
